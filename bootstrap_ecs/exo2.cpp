c982a92b70b489d07a4fcfd5a2411fd83dce0c86
JohannSako <johann.sako@epitech.eu>
2023-01-22 20:30:28 +0100

    [UPDATE] move class into an header file

--- bootstrap_ecs/exo2.cpp
+++ bootstrap_ecs/exo2.cpp
@@ -44,65 +44,6 @@ class C {
         std::string _value;
 };
 
-class ComponentManager {
-    public:
-        entity_t spawn_entity()
-        {
-            entity_t new_entity = -1;
-
-            for (auto &entity : _entity_status) {
-                if (entity.second == EntityStatus::DEAD) {
-                    new_entity = entity.first;
-                    entity.second = EntityStatus::USED;
-                    break;
-                }
-            }
-            if (new_entity == -1)
-                new_entity = (--_entity_status.end())->first + 1;
-            _entity_status[new_entity] = EntityStatus::ALIVE;
-            return new_entity;
-        }
-
-        void kill_entity(const entity_t &entity)
-        {
-            for (auto &components : _components)
-                components.second->delete_entity_components(entity);
-            _entity_status[entity] = EntityStatus::DEAD;
-        }
-
-        template <class Component>
-        void register_component(ComponentMap<Component> new_component)
-        {
-            if (_components.count(std::type_index(typeid(Component))) == 0) {
-                _components[std::type_index(typeid(Component))] = std::make_shared<ComponentMap<Component>>(new_component);
-            } else {
-                std::cerr << "component map has already been registered" << std::endl;
-            }
-        }
-
-        template <class Component>
-        ComponentMap<Component> &get_components()
-        {
-            return (std::static_pointer_cast<ComponentMap<Component>&>(_components[std::type_index(typeid(Component))]));
-        }
-
-        template <class Component>
-        void remove_components(const entity_t &entity)
-        {
-            _components[std::type_index(typeid(Component))]->delete_entity_components(entity);
-        }
-
-    private:
-        enum class EntityStatus {
-            ALIVE,
-            DEAD,
-            USED
-        };
-
-        std::map<entity_t, EntityStatus> _entity_status;
-        std::unordered_map<std::type_index, std::shared_ptr<IComponentMap>> _components;
-};
-
 int main(void)
 {
     ComponentManager manager;
