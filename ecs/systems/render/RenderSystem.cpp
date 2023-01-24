6fe77a11b9bfbe3a0428efd3afb63c9a2b0f5e2c
JohannSako <johann.sako@epitech.eu>
2023-01-24 17:35:59 +0100

    [ADD] RenderSystem empty cpp file

---
+++ ecs/systems/render/RenderSystem.cpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2023
+** RenderSystem
+** File description:
+** RenderSystem
+*/
+
+#include "RenderSystem.hpp"
+
+RenderSystem::RenderSystem()
+{
+
+}
+
+RenderSystem::~RenderSystem()
+{
+
+}
+
+void init_rendering_components(const ComponentMap<Render> &render)
+{
+
+}
+
+void display_content(const ComponentMap<Render> &render)
+{
+
+}
+
+void destroy_rendering_components()
+{
+    
+}
