0a32eb3d5bffa6c62cfcfe038e4d52ddd6ba0d86
JohannSako <johann.sako@epitech.eu>
2023-01-24 17:37:06 +0100

    [ADD] Collision Component

---
+++ ecs/components/content/Collision.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2023
+** Collision
+** File description:
+** Collision
+*/
+
+#ifndef _Collision_
+    #define _Collision_
+
+    #include <string>
+    #include <vector>
+    #include <algorithm>
+
+    class Collision {
+        public:
+            Collision(const std::vector<std::string> &families = {}) : _families(families) {}
+            ~Collision();
+
+            const std::vector<std::string> &getFamilies() const { return _families; }
+            void setFamilies(const std::vector<std::string> &families) { _families = families; }
+            bool isColliding(const std::string &family) { return std::find(_families.begin(), _families.end(), family) != _families.end(); }
+        private:
+            std::vector<std::string> _families;
+    };
+
+#endif /* !_Collision_ */
