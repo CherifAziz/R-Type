2f1bf9c1e5989649e14bc06261a211da3292bd25
JohannSako <johann.sako@epitech.eu>
2023-01-24 17:35:36 +0100

    [ADD] RenderSystem basic files

--- ecs/systems/render/RenderSystem.hpp
+++ ecs/systems/render/RenderSystem.hpp
@@ -8,7 +8,14 @@
 #ifndef _RenderSystem_
     #define _RenderSystem_
 
+    #include <SFML/Graphics.hpp>
+    #include <SFML/Audio.hpp>
+
+    #include <unordered_map>
+    #include <memory>
+
     #include "ARenderSystem.hpp"
+    #include "Entity.hpp"
 
     class RenderSystem : public ARenderSystem {
         public:
@@ -19,7 +26,13 @@
             void display_content(const ComponentMap<Render> &render);
             void destroy_rendering_components();
         private:
+            std::unordered_map<std::string, sf::Texture *> _textureCache;
+            std::unordered_map<std::shared_ptr<Entity>, sf::Sprite *> _spriteCache;
 
+            // std::unordered_map<std::string, sf::Font *> _fontCache;
+            // std::unordered_map<std::shared_ptr<Entity>, sf::Text *> _textCache;
+
+            // std::unordered_map<std::string, sf::Music *> _musicCache;
     };
 
 #endif /* !_RenderSystem_ */
