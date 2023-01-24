e82f5d9720eb551aa6194a70892ea1814ede38f9
JohannSako <johann.sako@epitech.eu>
2023-01-24 22:50:30 +0100

    [ADD] scale in sprite and text

--- ecs/components/content/Sprite.hpp
+++ ecs/components/content/Sprite.hpp
@@ -12,23 +12,28 @@
 
     class Sprite {
         public:
-            Sprite(const std::string &filename = "", const ssize_t &x = 0, const ssize_t &y = 0) : _filename(filename), _x(x), _y(y) {};
+            Sprite(const std::string &filename = "", const ssize_t &x = 0, const ssize_t &y = 0, const size_t &scale = 1) : _filename(filename), _x(x), _y(y), _scale(scale) {};
             ~Sprite(){};
 
-            const std::string &get_filename() const { return _filename; }
-            void set_filename(const std::string &filename) { _filename = filename;}
+            const std::string &getFilename() const { return _filename; }
+            void setFilename(const std::string &filename) { _filename = filename;}
 
-            const ssize_t &get_x() const {return _x;};
-            const ssize_t &get_y() const {return _y;};
-            void set_position(const ssize_t &x, const ssize_t &y)
+            const ssize_t &getX() const {return _x;};
+            const ssize_t &getY() const {return _y;};
+            void setPosition(const ssize_t &x, const ssize_t &y)
             {
                 _x = x;
                 _y = y;
             }
+
+            const size_t &getScale() const { return _scale; }
+            void setScale(const size_t &scale) { _scale = scale; }
+
         private:
             std::string _filename;
             ssize_t _x;
             ssize_t _y;
+            size_t _scale;
     };
 
 #endif /* !_Sprite_ */
--- ecs/components/content/Text.hpp
+++ ecs/components/content/Text.hpp
@@ -14,18 +14,22 @@
     class Text {
         public:
             typedef struct rgb_s {
+                rgb_s() : r(255), g(255), b(255) {}
                 uint16_t r;
                 uint16_t g;
                 uint16_t b;
             } rgb_t;
 
-            Text(const std::string &text, const std::string &font, const uint16_t &r = 255, const uint16_t &g = 255, const uint16_t &b = 255) : _text(text), _font(font)
+            Text(const std::string &text, const std::string &font, const ssize_t &x = 0, const ssize_t &y = 0, const size_t &characterSize = 30,
+            const size_t &scale = 1, const uint16_t &r = 255, const uint16_t &g = 255, const uint16_t &b = 255) : _text(text), _font(font), _x(x), _y(y),
+            _characterSize(characterSize), _scale(scale)
             {
                 _color.r = r;
                 _color.g = g;
                 _color.b = b;
             }
-            Text(const std::string &text, const std::string &font, const rgb_t &color) : _text(text), _font(font), _color(color) {}
+            Text(const std::string &text, const std::string &font, const ssize_t &x = 0, const ssize_t &y = 0, const size_t &characterSize = 30,
+            const size_t &scale = 1, const rgb_t &color = rgb_s()) : _text(text), _font(font), _color(color) {}
             ~Text();
 
             const std::string &getText() const { return _text; }
@@ -35,9 +39,26 @@
             const rgb_t &getColor() const { return _color; }
             void setColor(const rgb_t &color) { _color = color; }
 
+            const ssize_t &getX() const { return _x; };
+            const ssize_t &getY() const { return _y; };
+            void setPosition(const ssize_t &x, const ssize_t &y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            const size_t &getCharacterSize() const { return _characterSize; }
+            void setCharacterSize(const size_t &characterSize) { _characterSize = characterSize; }
+            const size_t &getScale() const { return _scale; }
+            void setScale(const size_t &scale) { _scale = scale; }
+
         private:
             std::string _text;
             std::string _font;
+            ssize_t _x;
+            ssize_t _y;
+            size_t _characterSize;
+            size_t _scale;
             rgb_t _color;
     };
 
