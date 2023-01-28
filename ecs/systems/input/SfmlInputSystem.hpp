c32979ff1e7f143c493e469dc5b9d4c99910d17d
JohannSako <johann.sako@epitech.eu>
2023-01-28 22:53:23 +0100

    [ADD] functionnal input system

--- ecs/systems/input/SfmlInputSystem.hpp
+++ ecs/systems/input/SfmlInputSystem.hpp
@@ -10,15 +10,47 @@
 
     #include <SFML/Graphics.hpp>
 
+    #include <memory>
+
     #include "AInputSystem.hpp"
+
+    #include "Movement.hpp"
 
     class SfmlInputSystem : public AInputSystem {
         public:
             SfmlInputSystem() : AInputSystem("Sfml") {}
-            ~SfmlInputSystem();
+            ~SfmlInputSystem() = default;
 
-            
+            void update(sf::RenderWindow &window, std::shared_ptr<ComponentMap<Movement>> movement)
+            {
+                while (window.pollEvent(_event)) {
+                    if (_event.type == sf::Event::Closed)
+                        window.close();
+                    else if (_event.type == sf::Event::KeyPressed || _event.type == sf::Event::KeyReleased)
+                        handleKey(_event.type, _event.key.code, movement->getFromIndex(0));
+                }
+            }
+
         protected:
+            sf::Event _event;
+
+            void handleKey(sf::Event::EventType event, sf::Keyboard::Key key, Movement &movement)
+            {
+                if (_keyTranslator.count(key) == 0)
+                    return;
+
+                if (event == sf::Event::KeyPressed)
+                    movement.setState(_keyTranslator[key], Movement::KeyState::PRESSED);
+                else if (event == sf::Event::KeyReleased)
+                    movement.setState(_keyTranslator[key], Movement::KeyState::RELEASED);
+            }
+
+            std::unordered_map<sf::Keyboard::Key, Movement::KeyType> _keyTranslator = {
+                {sf::Keyboard::Z, Movement::KeyType::Z},
+                {sf::Keyboard::S, Movement::KeyType::S},
+                {sf::Keyboard::Q, Movement::KeyType::Q},
+                {sf::Keyboard::D, Movement::KeyType::D}
+            };
     };
 
 #endif /* !_SfmlInputSystem_ */
