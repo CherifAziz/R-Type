5aeff665958cefc908008adca562a0d20055a967
JohannSako <johann.sako@epitech.eu>
2023-01-29 18:39:13 +0100

    [UPDATE] x and y value in Sprite now as ssize_t

--- ecs/components/content/Sprite.hpp
+++ ecs/components/content/Sprite.hpp
@@ -12,15 +12,15 @@
 
     class Sprite {
         public:
-            Sprite(const std::string &filename = "", const size_t &x = 0, const size_t &y = 0, const size_t &scale = 1) : _filename(filename), _x(x), _y(y), _scale(scale) {};
+            Sprite(const std::string &filename = "", const ssize_t &x = 0, const ssize_t &y = 0, const size_t &scale = 1) : _filename(filename), _x(x), _y(y), _scale(scale) {};
             ~Sprite() = default;
 
             const std::string &getFilename() const { return _filename; }
             void setFilename(const std::string &filename) { _filename = filename;}
 
-            const size_t &getX() const {return _x;};
-            const size_t &getY() const {return _y;};
-            void setPosition(const size_t &x, const size_t &y)
+            const ssize_t &getX() const {return _x;};
+            const ssize_t &getY() const {return _y;};
+            void setPosition(const ssize_t &x, const ssize_t &y)
             {
                 _x = x;
                 _y = y;
@@ -36,8 +36,8 @@
 
         protected:
             std::string _filename;
-            size_t _x;
-            size_t _y;
+            ssize_t _x;
+            ssize_t _y;
             size_t _scale;
     };
 
