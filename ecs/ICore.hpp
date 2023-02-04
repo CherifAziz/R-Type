de1e8ac837c098f9b40f3622f2467d073286eaaf
JohannSako <johann.sako@epitech.eu>
2023-02-04 09:08:10 +0100

    [ADD] Core in the architecture

---
+++ ecs/ICore.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** ICore.hpp
+** File description:
+** ICore structure
+*/
+
+#ifndef ICore
+    #define ICoreDef
+
+    namespace rtype {
+        /**
+         * @brief the Interface Core class
+         * 
+         */
+        class ICore
+        {
+            public:
+                /**
+                 * @brief Destroy the ICore object
+                 * 
+                 */
+                virtual ~ICore() = default;
+
+                /**
+                 * @brief the game loop
+                 * 
+                 * @return int the game return value (84 if an error occured, 0 otherwise)
+                 */
+                virtual int loopGame() = 0;
+        };
+    }
+
+#endif /* !ICoreDef */
