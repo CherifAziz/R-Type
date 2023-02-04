1212bae56d0b6f4a06e64a4cbb222084bab97e06
JohannSako <johann.sako@epitech.eu>
2023-02-04 09:09:46 +0100

    [ADD] Scene in the architecture

---
+++ ecs/scene/AScene.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2023
+** AScene
+** File description:
+** AScene
+*/
+
+#ifndef _AScene_
+    #define _AScene_
+
+    #include "IScene.hpp"
+
+    namespace rtype {
+        /**
+         * @brief the Abstract for Scene class
+         * 
+         */
+        class AScene : public IScene {
+            public:
+                /**
+                 * @brief Get the Component Manager object
+                 * 
+                 * @return the component manager as a ComponentManager& 
+                 */
+                ComponentManager &getComponentManager() { return _componentManager; }
+
+                /**
+                 * @brief Get the Entity Manager object
+                 * 
+                 * @return the entity manager as a EntityManager& 
+                 */
+                EntityManager &getEntityManager() { return _entityManager; }
+
+            protected:
+                /**
+                 * @brief the component manager
+                 * 
+                 */
+                ComponentManager _componentManager;
+
+                /**
+                 * @brief the entity manager
+                 * 
+                 */
+                EntityManager _entityManager;
+        };
+    }
+
+#endif /* !_AScene_ */
