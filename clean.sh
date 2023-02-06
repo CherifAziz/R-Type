4f26de6f36194cf6bfdc991cc7b4e9641f4080b8
Baptiste ALLEX <baptiste.allex@epitech.eu>
2023-02-06 19:51:56 +0100

    [MOD] compile with final version of johann

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf r-type_server r-type_client tcpclient tcpserver udpserver
+rm -rf r-type_server r-type_client tcpclient tcpserver udpserver rtype
--- install.bat
+++ install.bat
@@ -1,31 +1,30 @@
-@REM @echo off
+@echo off
 
-@REM if not "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-@REM   echo "Ce script doit être exécuté sur un système 64-bits."
-@REM   exit /b 1
-@REM )
+if not "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  echo "Ce script doit être exécuté sur un système 64-bits."
+  exit /b 1
+)
 
-@REM git --version || (winget install -e --id Git.Git)
+git --version || (winget install -e --id Git.Git)
 
-@REM cmake --version || (winget install -e --id Kitware.CMake)
+cmake --version || (winget install -e --id Kitware.CMake)
 
-@REM rem if not exist "%CD%\vcpkg" (
-@REM rem   echo putepute
-@REM rem   git clone https://github.com/Microsoft/vcpkg.git
-@REM rem   cd vcpkg
-@REM rem   .\bootstrap-vcpkg.bat
-@REM rem ) else (
-@REM rem   cd vcpkgc
-@REM rem   git pull
-@REM rem   call .\bootstrap-vcpkg.bat
-@REM rem   cd ..
-@REM rem )
+if not exist "%CD%\vcpkg" (
+  git clone https://github.com/Microsoft/vcpkg.git
+  cd vcpkg
+  .\bootstrap-vcpkg.bat
+) else (
+  cd vcpkgc
+  git pull
+  call .\bootstrap-vcpkg.bat
+  cd ..
+)
 
-@REM call vcpkg\vcpkg.exe install boost-asio
-@REM call vcpkg\vcpkg.exe install sfml
-@REM call vcpkg\vcpkg.exe integrate install
+call vcpkg\vcpkg.exe install boost-serialization
+call vcpkg\vcpkg.exe install boost-system
+call vcpkg\vcpkg.exe install sfml
+call vcpkg\vcpkg.exe integrate install
 cmake -S . -B .\build\ -DCMAKE_TOOLCHAIN_FILE="%CD%\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
 cmake --build .\build\ --config Release
 move assets build\Release\assets
-mklink /H ".\r-type_client.lnk" "%CD%\build\Release\r-type_client.exe"
-mklink /H ".\r-type_server.lnk" "%CD%\build\Release\r-type_server.exe"
+mklink /H ".\rtype.lnk" "%CD%\build\Release\rtype.exe"
