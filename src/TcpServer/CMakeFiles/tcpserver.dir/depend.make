fc154883f730cce0de14bfae07ad04acec81f10f
JohannSako <johann.sako@epitech.eu>
2023-02-06 15:55:07 +0100

    [ADD] CMake for sources

--- src/TcpServer/CMakeFiles/tcpserver.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tcpserver.
-# This may be replaced when dependencies are built.
--- src/TcpServer/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "TcpServer.hpp"
-
-int main()
-{
-    boost::asio::io_context io_context;
-    TcpServer server(io_context, 3333);
-    io_context.run();
-    return 0;
-}
