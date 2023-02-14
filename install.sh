f564f0382f2dcad324b97a4ff8732ebefbe4968d
Baptiste ALLEX <baptiste.allex@epitech.eu>
2023-02-14 16:02:53 +0100

    [MOD] Cpack working v0.1

--- install.sh
+++ install.sh
@@ -4,15 +4,22 @@ if [ -d "$PWD/build" ]; then
   rm -rf $PWD/build
 fi
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Veuillez lancer ce script en tant que root."
-  exit 1
+if [ -e "$PWD/rtype" ]; then
+  rm -rf $PWD/rtype
+fi
+
+if [ -e "$PWD/rtype-1.0.zip" ]; then
+  rm -rf $PWD/rtype-1.0.zip
+fi
+
+if [ -e "$PWD/rtype-1.0-Source.zip" ]; then
+  rm -rf $PWD/rtype-1.0-Source.zip
 fi
 
 if ! command -v ping > /dev/null; then
   echo "ping n'est pas installé, installation en cours..."
-  yum update -y
-  yum install -y iputils
+  sudo yum update -y
+  sudo yum install -y iputils
 else
   echo "ping déjà installé."
 fi
@@ -25,7 +32,7 @@ fi
 if ! command -v g++ > /dev/null; then
   echo "g++ n'est pas installé, installation en cours..."
   curl -L https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz -o gcc.tar.gz
-  tar -xzf gcc.tar.gz
+  sudo tar -xzf gcc.tar.gz
   cd gcc-12.2.0
   ./configure
   make
@@ -38,9 +45,9 @@ fi
 if ! [ -d "$PWD/vcpkg" ]; then
   echo "vcpkg n'est pas installé, installation en cours..."
   git clone https://github.com/Microsoft/vcpkg.git
-  $PWD/vcpkg/bootstrap-vcpkg.sh -disableMetrics
-  cp $PWD/vcpkg/vcpkg /usr//bin/vcpkg
-  chmod +x /usr/bin/vcpkg
+  sudo $PWD/vcpkg/bootstrap-vcpkg.sh -disableMetrics
+  sudo cp $PWD/vcpkg/vcpkg /usr//bin/vcpkg
+  sudo chmod +x /usr/bin/vcpkg
 else 
   echo "vcpkg déjà installé."
 fi
@@ -54,36 +61,40 @@ fi
 if ! command -v cmake > /dev/null; then
   echo "cmake n'est pas installé, installation en cours..."
   curl -L https://github.com/Kitware/CMake/releases/download/v3.25.0-rc1/cmake-3.25.0-rc1-linux-x86_64.sh -o cmake-install.sh
-  chmod +x cmake-install.sh
-  $PWD/cmake-install.sh --skip-license
-  rm cmake-install.sh
+  sudo chmod +x cmake-install.sh
+  sudo $PWD/cmake-install.sh --skip-license
+  sudo rm cmake-install.sh
   echo "cmake installé."
 else
   echo "cmake déjà installé."
 fi
 
 cd $PWD/vcpkg
-git pull
-./bootstrap-vcpkg.sh -disableMetrics
+sudo git pull
+sudo ./bootstrap-vcpkg.sh -disableMetrics
 cd ..
-$PWD/vcpkg/vcpkg install sfml
-$PWD/vcpkg/vcpkg install boost-system
-$PWD/vcpkg/vcpkg install boost-serialization
-$PWD/vcpkg/vcpkg upgrade
-if ! [ -e "/root/.vcpkg" ]; then
-  mkdir /root/.vcpkg
-fi
-if ! [ -e "/root/.vcpkg/vcpkg.path.txt" ]; then
-  touch /root/.vcpkg/vcpkg.path.txt
+sudo $PWD/vcpkg/vcpkg install sfml
+sudo $PWD/vcpkg/vcpkg install boost-system
+sudo $PWD/vcpkg/vcpkg install boost-serialization
+sudo $PWD/vcpkg/vcpkg upgrade
+
+if sudo [ -e "/root/.vcpkg" ]; then
+  echo "/root/.vcpkg existe déjà."
+else
+  sudo mkdir /root/.vcpkg
 fi
 
-$PWD/vcpkg/vcpkg integrate install
+if sudo [ -e "/root/.vcpkg/vcpkg.path.txt" ]; then
+  echo "/root/.vcpkg/vcpkg.path.txt existe déjà."
+else
+  sudo touch /root/.vcpkg/vcpkg.path.txt
+fi
+
+sudo $PWD/vcpkg/vcpkg integrate install
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=$PWD/build/
 cmake --build ./build --config Release
-mv build/bin/* $PWD/
-# cd build
-# cpack --config CPackConfig.cmake -G TGZ
-# cpack --config CPackConfig.cmake -G ZIP
-# cpack --config CPackSourceConfig.cmake -G TGZ
-# cpack --config CPackSourceConfig.cmake -G ZIP
-# cd ..
+cd build
+cpack --config CPackConfig.cmake -G ZIP
+cpack --config CPackSourceConfig.cmake -G ZIP
+cd ..
+mv build/*.zip $PWD
