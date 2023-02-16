fbbc3bcdb11551d315d1828405ac64c11047685e
Baptiste ALLEX <baptiste.allex@epitech.eu>
2023-02-16 15:57:14 +0100

    [MOD] adding .zip to cleaning

--- clean.bat
+++ clean.bat
@@ -4,3 +4,8 @@ if exist ".\build" (
   rmdir /s /q .\build
   echo "Suppression du dossier build"
 )
+
+if exist ".\rtype-1.0-win32.zip" (
+  del .\rtype-1.0-win32.zip
+  echo "Suppression du fichier zip"
+)
--- clean.sh
+++ clean.sh
@@ -3,3 +3,7 @@
 if [ -d "$PWD/build" ]; then
   rm -rf $PWD/build
 fi
+
+if [ -d "$PWD/rtype-1.0-Linux" ]; then
+  rm -rf $PWD/rtype-1.0-Linux
+fi
