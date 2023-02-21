cf59cba63dd717cbda72e692fbaa2a8af089ddf6
Baptiste ALLEX <baptiste.allex@epitech.eu>
2023-02-21 11:12:40 +0100

    [MOD] update compilation directory, adding clean on build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+./clean.sh
+
 cmake -B ./build/ -DCMAKE_TOOLCHAIN_FILE=$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build ./build/ --config Release
 cmake --build ./build/ --target package
