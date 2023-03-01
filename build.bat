ddaed21b4523e1c30465421cf7f86a2b37b34b52
CherifAziz <72012855+CherifAziz@users.noreply.github.com>
2023-03-01 16:57:33 +0100

    [FIX] - try to fix clock

--- build.bat
+++ build.bat
@@ -1,12 +1,9 @@
 @echo off
 
-@REM call .\clean.bat
+call .\clean.bat
 
 cmake -S . -B .\build\ -DCMAKE_TOOLCHAIN_FILE=%cd%\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .\build\ --config Release --target package
-@REM cd build
-@REM cpack --config CPackConfig.cmake -G ZIP
-@REM cd ..
 if not exist ".\build\Release\assets\" (
   mklink /j .\build\Release\assets\ .\assets\
 )
