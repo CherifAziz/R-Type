58b7f409b95528c7c108f3d7485c9154e58e5a5d
Remy <remy.noulin@epitech.eu>
2023-03-01 17:48:39 +0100

    [MOD] adaptation to tcp

--- Client/Core.cpp
+++ Client/Core.cpp
@@ -15,7 +15,7 @@
 
 #include "SfmlInputSystem.hpp"
 #include "SfmlRenderSystem.hpp"
-#include "UdpClientSystem.hpp"
+#include "TcpClientSystem.hpp"
 #include "Services.hpp"
 
 Core::Core(boost::asio::io_context &ioc, std::string ip, std::string port) : _timer(ioc)
@@ -23,7 +23,7 @@ Core::Core(boost::asio::io_context &ioc, std::string ip, std::string port) : _ti
     _systems.push_back(std::make_shared<SfmlInputSystem>());
     _systems.push_back(std::make_shared<SfmlRenderSystem>());
     _systems.push_back(std::make_shared<RTypeGameSystem>(this->_scenes));
-    _systems.push_back(std::make_shared<UdpClientSystem>(ioc, ip, port, std::make_shared<Services::Service>()));
+    _systems.push_back(std::make_shared<TcpClientSystem>(ioc, ip, std::atoi(port.c_str()), std::make_shared<Services::Service>()));
     for (auto &system : _systems)
         system->init();
 }
@@ -39,8 +39,9 @@ int Core::loopGame()
     if (this->isGameRunning()) {
         this->_timer.expires_after(std::chrono::milliseconds(1000 / 60));
         this->_timer.async_wait(boost::bind(&Core::loopGame, this));
-        for (auto &system : _systems)
+        for (auto &system : _systems) {
             system->update(_scenes[system->getCurrentScene()]);
+        }
     } else {
         for (auto &system : this->_systems)
             system->destroy();
